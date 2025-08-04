a42fc4f02a0679eaaec2446b41ce63a885607f93
Micaela Svatzky <svatzkymicaela@gmail.com>
2025-08-04 17:10:45 -0300

    fix vercel deploy

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -9,13 +9,18 @@ import LocationGrid from "@/components/LocationGrid";
 import Loading from "@/components/Loading";
 
 const SearchPage = () => {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("q")?.toLowerCase() || "";
+  const [query, setQuery] = useState("");
 
   const [loading, setLoading] = useState(false);
   const [characters, setCharacters] = useState([]);
   const [episodes, setEpisodes] = useState([]);
   const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const q = params.get("q")?.toLowerCase() || "";
+    setQuery(q);
+  }, []);
 
   useEffect(() => {
     if (!query) {
@@ -54,59 +59,66 @@ const SearchPage = () => {
     fetchData();
   }, [query]);
 
-  
-
   return (
     <div>
-    {!loading && (
-    <div
-      className="min-h-screen text-white relative overflow-hidden"
-      style={{
-        background: `
+      {!loading && (
+        <div
+          className="min-h-screen text-white relative overflow-hidden"
+          style={{
+            background: `
           radial-gradient(circle at 20% 30%, rgba(255, 0, 255, 0.2) 0%, transparent 30%),
           radial-gradient(circle at 80% 20%, rgba(0, 255, 255, 0.2) 0%, transparent 40%),
           radial-gradient(circle at 50% 80%, rgba(173, 216, 230, 0.15) 0%, transparent 50%),
           linear-gradient(135deg, #1a0023 0%, #10002b 50%, #000014 100%)
         `,
-        backdropFilter: "blur(8px)",
-      }}
-    >
-      <PagesHeroSection title={`Search results for: ${query}`} />
+            backdropFilter: "blur(8px)",
+          }}
+        >
+          <PagesHeroSection title={`Search results for: ${query}`} />
 
-      <section className="mb-5">
-        <div className="flex flex-col justify-center items-center">
-          <h2 className="text-4xl text-amber-100 font-semibold mt-5">Characters</h2>
-          {characters.length > 0 ? (
-            <CharacterGrid characters={characters} />
-          ) : (
-            <p className="text-white text-2xl mt-4 col-span-full">No results</p>
-          )}
-        </div>
-      </section>
+          <section className="mb-5">
+            <div className="flex flex-col justify-center items-center">
+              <h2 className="text-4xl text-amber-100 font-semibold mt-5">
+                Characters
+              </h2>
+              {characters.length > 0 ? (
+                <CharacterGrid characters={characters} />
+              ) : (
+                <p className="text-white text-2xl mt-4 col-span-full">
+                  No results
+                </p>
+              )}
+            </div>
+          </section>
 
-      <section className="mb-5">
-        <div className="flex flex-col justify-center items-center">
-          <h2 className="text-4xl text-amber-100 font-semibold mt-5">Locations</h2>
-          {locations.length > 0 ? (
-            <LocationGrid locations={locations} />
-          ) : (
-            <p className="text-white text-2xl mt-4">No results</p>
-          )}
-        </div>
-      </section>
+          <section className="mb-5">
+            <div className="flex flex-col justify-center items-center">
+              <h2 className="text-4xl text-amber-100 font-semibold mt-5">
+                Locations
+              </h2>
+              {locations.length > 0 ? (
+                <LocationGrid locations={locations} />
+              ) : (
+                <p className="text-white text-2xl mt-4">No results</p>
+              )}
+            </div>
+          </section>
 
-      <section className="mb-5">
-        <div className="flex flex-col justify-center items-center">
-          <h2 className="text-4xl text-amber-100 font-semibold mt-5">Episodes</h2>
-          {episodes.length > 0 ? (
-            <EpisodeGrid episodes={episodes} />
-          ) : (
-            <p className="text-white text-2xl mt-4">No results</p>
-          )}
+          <section className="mb-5">
+            <div className="flex flex-col justify-center items-center">
+              <h2 className="text-4xl text-amber-100 font-semibold mt-5">
+                Episodes
+              </h2>
+              {episodes.length > 0 ? (
+                <EpisodeGrid episodes={episodes} />
+              ) : (
+                <p className="text-white text-2xl mt-4">No results</p>
+              )}
+            </div>
+          </section>
         </div>
-      </section>
-    </div> )}
-    {loading && <Loading />}
+      )}
+      {loading && <Loading />}
     </div>
   );
 };
