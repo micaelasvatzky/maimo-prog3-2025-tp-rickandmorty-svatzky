29f0ff940d180deb8c1687f617f3dafb64f69191
Micaela Svatzky <svatzkymicaela@gmail.com>
2025-08-05 10:57:05 -0300

    add suspense

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,12 +1,13 @@
-"use client"
-
+import Loading from "@/components/Loading";
 import SearchComp from "@/components/SearchComp";
-
+import { Suspense } from "react";
 
 const Search = () => {
   return (
-    <SearchComp />
-  )
-}
+    <Suspense fallback={<Loading />}>
+      <SearchComp />
+    </Suspense>
+  );
+};
 
 export default Search;
